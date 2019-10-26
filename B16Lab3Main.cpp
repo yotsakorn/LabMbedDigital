eb0cab9cefc682e2035553ac6c40d1df7bdf1865
yotsakorn <t.yotsakorn@gmail.com>
2019-10-26 14:46:01 +0700

    Update B16Lab3Main.cpp

--- B16Lab3Main.cpp
+++ B16Lab3Main.cpp
@@ -15,8 +15,6 @@ int main()
   uint8_t input_user = 0; 
   uint8_t input_server = 0;
 
-  uint64_t nextLedTimeUs = 0;
-  uint64_t ledPeriodUs = 500000;
 
   ///////////////////////////////******* Loop *********//////////////////////////////////////////
 
@@ -62,12 +60,27 @@ int main()
 
       case '1':
         server_side.printf("Request : 7-Segment Show Number 0-9\n");
-        server_side.printf("Enter Y or etc. : ");
-        while(server_side.readable() == 0)
+        server_side.printf("Enter Y or etc. : \r");
+        STATE_Display = 12;
+        break;
+
+      case '2':
+        server_side.printf("Request : Read Analog Value\n");
+        server_side.printf("Enter Y or etc. : \r");
+        STATE_Display = 13;
+        break;
+
+      default:
+        STATE_Display = 10;
+        break;
+      }
+      break;
+
+    case 12:
+      switch(input_server)
         {
-        }
-        switch(input_server)
-        {
+          case 0:
+            break;
           case 'Y':
             server_side.printf("----------------------\n");
             server_side.printf("Access Complete\n");
@@ -81,21 +94,18 @@ int main()
             STATE_Display = 19;
             break;
         }
-        break;
+      break;
 
-      case '2':
-        server_side.printf("Request : Read Analog Value\n");
-        server_side.printf("Enter Y or etc. : ");
-        while(server_side.readable() == 0)
+      case 13:
+      switch(input_server)
         {
-        }
-        switch(input_server)
-        {
+          case 0:
+            break;
           case 'Y':
             server_side.printf("----------------------\n");
             server_side.printf("Access Complete\n");
             server_side.printf("----------------------\n");
-            STATE_Display = 30;
+            STATE_Display = 40;
             break;
           default:
             server_side.printf("----------------------\n");
@@ -104,13 +114,8 @@ int main()
             STATE_Display = 19;
             break;
         }
-        break;
-
-      default:
-        STATE_Display = 19;
-        break;
-      }
       break;
+      
 
     ///////////////////////////////////////////////////////////////////////////////////////
     case 19: //error state
@@ -135,75 +140,60 @@ int main()
     case 21:
       switch(input_user)
       {
+        case 0:
+          break;
+
         case '0':
-          server_side.printf("Request : Number 0");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 0;
-          STATE_Display = 10;
+          server_side.printf("Request : Number 0 \n");
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 22;
           break;
+         
         case '1':
-          server_side.printf("Request : Number 1");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 1;
-          STATE_Display = 10;
+          server_side.printf("Request : Number 1 \n");
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 23;
           break;
+
         case '2':
           server_side.printf("Request : Number 2");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 2;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 24;
           break;
         case '3':
           server_side.printf("Request : Number 3");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 3;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 25;
           break;
         case '4':
           server_side.printf("Request : Number 4");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 4;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 26;
           break;
         case '5':
           server_side.printf("Request : Number 5");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 5;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 27;
           break;
         case '6':
           server_side.printf("Request : Number 6");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 6;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 28;
           break;
         case '7':
           server_side.printf("Request : Number 7");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 7;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 29;
           break;
         case '8':
           server_side.printf("Request : Number 8");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 8;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 30;
           break;
         case '9':
           server_side.printf("Request : Number 9");
-          while(input_server != 'Y')
-          {}
-          STATE_number = 9;
-          STATE_Display = 10;
+          server_side.printf("Enter Y or etc. : \n");
+          STATE_Display = 31;
           break;
         default:
           user_side.printf("Please Enter Number 0-9\n");
@@ -212,19 +202,205 @@ int main()
       }
       break;
 
+      /////////////******************************************//////////////////////
+
+    case 22:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 0;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+
+    case 23:
+        switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 1;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+    case 24:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 2;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+
+    case 25:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 3;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+
+    case 26:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 4;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+    
+    case 27:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 5;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+
+    case 28:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 6;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+
+    case 29:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 7;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+    
+    case 30:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 8;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
+    
+    case 31:
+      switch(input_server)
+      {
+        case 0:
+          break;
+
+        case 'Y':
+          STATE_number = 9;
+          STATE_Display = 10;
+          break;
+      
+        default:
+          user_side.printf("Please Enter Number 0-9\n");
+          STATE_Display = 21;
+          break;
+
+      }
+      break;
 
     /////////////////////////////////////////////////////////////////////////////////////////
-    case 30: //display state
-      user_side.printf("----------------------\n");
-      user_side.printf("Access Complete\n");
-      user_side.printf("----------------------\n");
-      while(input_user == 0)
-      {
-        value_read = analog_value.read();
-        value_read = value_read*3.3;
-        user_side.printf("Analog value read = %4.2f V.\n",value_read);
-      }
+    case 40: //display state
+      value_read = analog_value.read();
+      value_read = value_read*3.3;
+      user_side.printf("Analog value read = %4.2f V.\n",value_read);
       STATE_Display = 10;
       break;
+
   }
+}
 }
